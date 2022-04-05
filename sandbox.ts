51c8ac993311ec9d3fcce0f73d62295baf27038b
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-05 22:52:08 +0430

    get start with typeScript

---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const myName = 'Benyamin';
+console.log(myName);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs)
+
+inputs.forEach(input => {
+    console.log(input);
+});
