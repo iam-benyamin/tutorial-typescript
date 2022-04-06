47460015161dc12af0d36911723520fbe1be19ab
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-06 23:56:03 +0430

    basic type and control input value for entry vars

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,15 @@
 var myName = 'Benyamin';
-console.log(myName);
+var age = 21;
+var isBlackBelt = false;
 var inputs = document.querySelectorAll('input');
-console.log(inputs);
+// in typescript we can not assign value with diffrent type to a variable
+// type 'string' is not assignable to type 'number'.
+// age = 'sara';
 inputs.forEach(function (input) {
     console.log(input);
 });
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circ("radius value"));
+console.log(circ(4));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,22 @@
 const myName = 'Benyamin';
-console.log(myName);
-
+let age = 21;
+let isBlackBelt = false;
 const inputs = document.querySelectorAll('input');
-console.log(inputs)
+
+// in typescript we can not assign value with diffrent type to a variable
+
+// type 'string' is not assignable to type 'number'.
+// age = 'sara';
 
 inputs.forEach(input => {
     console.log(input);
 });
+
+// in typescript we cat forse function to get spesific input type by using ":"
+// end of the variable like this example  
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+// console.log(circ("radius value"));
+console.log(circ(4));
