0da2522b495a47c35fd4d0c1f4fadfd75ac64c33
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-10 23:25:32 +0430

    array and object

--- sandbox.ts
+++ sandbox.ts
@@ -20,3 +20,35 @@ const circ = (diameter: number) => {
 
 // console.log(circ("radius value"));
 console.log(circ(4));
+
+// array 
+// in typescript the value of variable is fixed and also the variable is fixed to 
+// so if we define a property we can't change that.
+let names = ['sara', 'benyamin', 'milad'];
+names.push('ali');
+
+// Argument of type 'number' is not assignable to parameter of type 'string'.
+// names.push(5);
+
+let numbers = [1, 5, 7, 29, 83, 11];
+numbers.push(20);
+
+//  Argument of type 'string' is not assignable to parameter of type 'number'.
+// numbers.push('ahmad');
+
+let mixed = ['ali', 38, 'kam', 88, 11, '8u274', 5];
+mixed.push('milad');
+mixed.push(55);
+console.log(mixed)
+
+// object
+let person = {
+    name: 'Benyamin',
+    age: 21,
+    job: 'programmer',
+};
+
+person.age = 22;
+
+// Type 'string' is not assignable to type 'number'.
+// person.age = 'ale';
